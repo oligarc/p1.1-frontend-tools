67f4a82e14895d4adc88db6480468e5f5c6b1fe5
oligarc <oliver25399@gmail.com>
2024-09-20 13:04:43 +0200

    20/09/2024

---
+++ integer-script.js
@@ -0,0 +1,6 @@
+let n1 = 11;
+let n2 = 3;
+
+let rest = n1 % n2;
+let result = (n1 - rest) / n2;
+console.log(result)
---
+++ stringExercises.js
@@ -0,0 +1,44 @@
+let firstName = "Oliver";
+let lastName = "García";
+
+//CONCATENATION AND TEMPLATE LITERALS
+console.log("My name is " +firstName + " and my last name is " +lastName);
+console.log(`${firstName}  ${lastName}`);
+
+//STRING METHODS
+console.log(firstName.toUpperCase())
+console.log(firstName.toLowerCase())
+
+console.log(firstName.substring(0,2))
+console.log(lastName.slice(1,4))
+
+//STRING INTERPOLATION
+
+let fullData = `${firstName} 
+                ${lastName}
+                Rafael Alberti`
+
+console.log(fullData)
+
+//SPLITTING AND TRIMMING STRINGS
+
+let things = " Coco,        Margarita,Caritrini,Peter,Woody"
+console.log(things.trim())
+let thingSplits = things.trim().split(",")
+console.log(thingSplits)
+
+
+//CHECKING AND CONVERTING
+
+let tryVariable = "123";
+console.log(isNaN(tryVariable)) //False, so it's a number
+
+console.log(Number(tryVariable) +4) //Change 123 to number and add 4
+console.log(tryVariable+4) //If not, it is concatenated.
+
+let stringToNumber = Number(tryVariable)
+console.log(typeof(stringToNumber))
+console.log(typeof(stringToNumber.toString()))
+let toFixed = stringToNumber.toFixed()
+
+
